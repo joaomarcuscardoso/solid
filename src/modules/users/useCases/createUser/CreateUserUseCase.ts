cea1f7d9d688f868e3f2a2f265b5a9ced1797820
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-09-19 20:54:57 -0300

    createUser done

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,5 @@
+import { json } from "express";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,7 +12,18 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const usersAlreadyExist = this.usersRepository.findByEmail(email);
+
+    if (usersAlreadyExist) {
+      throw new Error("User already exist!");
+    }
+
+    const user = this.usersRepository.create({
+      email,
+      name,
+    });
+
+    return user;
   }
 }
 
